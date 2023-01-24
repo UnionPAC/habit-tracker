9911f91b933b49ccad604d13e400123cbc7dec6e
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro-2.local>
2023-01-24 10:37:40 -0500

    removed tracking for final ignore files

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const config = require("config");
-const db = config.get("mongoURI");
+// const db = config.get("mongoURI");
+require("dotenv").config();
+const mongoURI = process.env.mongoURI;
+const jwtSecret = process.env.jwtSecret;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(mongoURI);
     console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error(error);
