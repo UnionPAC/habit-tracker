46bbc9ff39ac4c6e8a1102a504c100c6d7ff35a3
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro-2.local>
2023-01-24 13:01:47 -0500

    set static folder fixed

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/habits", require("./routes/habits"));
 
 // set static folder
-app.use(express.static("/client/build"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html", (err) =>
+      res.status(500).send(err)
+    )
+  );
 });
 
 const PORT = process.env.PORT || 5000;
