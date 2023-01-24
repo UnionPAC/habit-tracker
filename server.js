b6dc5e7994d09e1a50da225637f159ac223bbaa4
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro-2.local>
2023-01-24 12:52:08 -0500

    fixed script issue, ready for cyclic deploy

--- server.js
+++ server.js
@@ -14,15 +14,12 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/habits", require("./routes/habits"));
 
-// serve static assets in prod
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("/client/build"));
+// set static folder
+app.use(express.static("/client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
