f002a1f8853c5dc5cceca0da22028ab70f7f815b
Geoff Jamieson <geoffjamieson@Geoffs-MacBook-Pro-2.local>
2023-01-24 12:34:13 -0500

    added build script

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 
 // Connect Database
 connectDB();
@@ -12,6 +13,16 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/habits", require("./routes/habits"));
+
+// serve static assets in prod
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("/client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
