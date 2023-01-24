33c70a9c3877909fbccedaadc059a4fc96dabaf3
geoff <geoff.ijamieson@gmail.com>
2023-01-24 15:40:17 -0500

    final changes, ready to deploy

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@
 const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
+const jwtSecret = config.get("jwtSecret");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db);
     console.log("MongoDB connected successfully!");
+    console.log(db);
+    console.log(jwtSecret);
   } catch (error) {
     console.error(error);
     process.exit(1);
