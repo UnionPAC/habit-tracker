2adb1255edbcc21dec323adabcb57ced0392e246
geoff <geoff.ijamieson@gmail.com>
2023-01-24 16:09:07 -0500

    fixed __dirname issue on resolve

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/api/habits", require("./routes/habits"));
 
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve("client", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
